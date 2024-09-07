0b2d52ba1baabb260aaf47d74c6901fe152acfa4
AJITH KUMAR <150109444+ajithkumar200513@users.noreply.github.com>
2024-09-07 15:57:36 +0530

    Delete web directory

--- web/api/Routes/AdminRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const route = express.Router()
-const {AdminSignup,AdminLogin} = require('../Controllers/AdminControllers')
-route.post('/signup',AdminSignup)
-route.post('/login',AdminLogin)
-
-
-module.exports = route
